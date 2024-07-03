3c42d46f17765ac3d3e9d9b6c3eab3134d247e47
Simon Ljungström <simonlj8@gmail.com>
2024-07-03 20:27:28 +0200

    Remove unnessecary code

--- src/main.js
+++ src/main.js
@@ -3,28 +3,23 @@ const { default: kaplay } = require("kaplay");
 
 const FLOOR_HEIGHT = 48;
 const JUMP_FORCE = 800;
-const SPEED = 480;
+const SPEED = 380;
 
-// initialize context
 kaplay();
 
-// load assets
 loadSprite("bean", "sprites/bean.png");
 
 scene("game", () => {
-    // define gravity
+
     setGravity(1600);
 
-    // add a game object to screen
     const player = add([
-        // list of components
         sprite("bean"),
         pos(80, 40),
         area(),
         body(),
     ]);
 
-    // floor
     add([
         rect(width(), FLOOR_HEIGHT),
         outline(4),
@@ -41,12 +36,10 @@ scene("game", () => {
         }
     }
 
-    // jump when user press space
     onKeyPress("space", jump);
     onClick(jump);
 
     function spawnTree() {
-        // add tree obj
         add([
             rect(48, rand(32, 96)),
             area(),
@@ -58,27 +51,21 @@ scene("game", () => {
             "tree",
         ]);
 
-        // wait a random amount of time to spawn next tree
         wait(rand(0.5, 1.5), spawnTree);
     }
 
     // start spawning trees
     spawnTree();
 
-    // lose if player collides with any game obj with tag "tree"
     player.onCollide("tree", () => {
-        // go to "lose" scene and pass the score
         go("lose", score);
-        burp();
+       // burp();
         addKaboom(player.pos);
     });
 
-    // keep track of score
     let score = 0;
 
     const scoreLabel = add([text(score), pos(24, 24)]);
-
-    // increment score every frame
     onUpdate(() => {
         score++;
         scoreLabel.text = score;
@@ -93,7 +80,7 @@ scene("lose", (score) => {
         anchor("center"),
     ]);
 
-    // display score
+
     add([
         text(score),
         pos(width() / 2, height() / 2 + 80),
@@ -101,7 +88,6 @@ scene("lose", (score) => {
         anchor("center"),
     ]);
 
-    // go back to game with space is pressed
     onKeyPress("space", () => go("game"));
     onClick(() => go("game"));
 });
